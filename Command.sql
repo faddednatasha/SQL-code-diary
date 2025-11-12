332b5a4f0cb0e8571d4a811e6708317b55f42da5
Aayushi Tewari <111345830+faddednatasha@users.noreply.github.com>
2025-11-12 23:16:47 +0530

    Basic Command.sql

---
+++ Command.sql
@@ -0,0 +1,13 @@
+-- SQL Basic Database Management Commands
+
+-- 1. Show all existing databases
+SHOW DATABASES;
+
+-- 2. Create a new database named 'my_new_db'
+CREATE DATABASE my_new_db;
+
+-- 3. Use the newly created database (or any existing database)
+USE my_new_db;
+
+-- 4. Drop (delete) a database named 'my_new_db'
+DROP DATABASE my_new_db;
